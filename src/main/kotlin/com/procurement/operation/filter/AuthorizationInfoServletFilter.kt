b4fa3ce55afaafea81e245fb8953170972514d66
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-02-14 12:35:12 +0200

    Appended HttpMethod & URI to MDC for log.

--- src/main/kotlin/com/procurement/operation/filter/AuthorizationInfoServletFilter.kt
+++ src/main/kotlin/com/procurement/operation/filter/AuthorizationInfoServletFilter.kt
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest
 class AuthorizationInfoServletFilter : Filter {
     companion object {
         private const val REMOTE_ADDRESS = "remoteAddr"
+        private const val HTTP_METHOD = "httpMethod"
+        private const val REQUEST_URI = "uri"
     }
 
     override fun init(arg0: FilterConfig) {}
@@ -18,12 +20,18 @@ class AuthorizationInfoServletFilter : Filter {
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         with(request as HttpServletRequest) {
             MDC.put(REMOTE_ADDRESS, remoteAddr)
+            MDC.put(HTTP_METHOD, method)
+
+            val uri = requestURI + queryString?.let { "?" + it }
+            MDC.put(REQUEST_URI, uri)
         }
 
         try {
             chain.doFilter(request, response)
         } finally {
             MDC.remove(REMOTE_ADDRESS)
+            MDC.remove(HTTP_METHOD)
+            MDC.remove(REQUEST_URI)
         }
     }
 }
